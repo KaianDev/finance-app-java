9bb001b654223d16a656bc42cd1ce54991e1ad90
Vitor Alves <vitor36alves@gmail.com>
2024-03-15 23:39:20 -0300

    feat: create search functionality in the repository

--- src/main/java/br/com/money/repository/ActivityRepository.java
+++ src/main/java/br/com/money/repository/ActivityRepository.java
@@ -1,15 +1,16 @@
 package br.com.money.repository;
 
 import br.com.money.model.Activity;
-import org.springframework.cglib.core.Local;
+import br.com.money.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
     List<Activity> findByDate(LocalDate localDate);
+    List<Activity> findByDateAndUser(LocalDate localDate, User user);
+    List<Activity> findAllActivitiesByUser(User user);
 }
