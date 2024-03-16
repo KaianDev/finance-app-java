06d537b79269ac49b18de8c6be51d25e88a20034
Vitor Alves <vitor36alves@gmail.com>
2024-03-15 23:28:01 -0300

    feat: create token convert service

--- src/main/java/br/com/money/service/TokenConvert.java
+++ src/main/java/br/com/money/service/TokenConvert.java
@@ -1,2 +1,11 @@
-package br.com.money.service;public class TokenConvert {
+package br.com.money.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenConvert {
+    public String convert(HttpServletRequest request) {
+        return request.getHeader("Authorization").replace("Bearer ", "");
+    }
 }
